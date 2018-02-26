03ad661625edf1c4a8784ae6ccd8333b7d2125a3
ignatandrei <ignatandrei@yahoo.com>
2018-02-26 02:11:46 +0200

    guid generator

--- LibQR/LibGenerateInfo/Controllers/HomeController.cs
+++ LibQR/LibGenerateInfo/Controllers/HomeController.cs
@@ -26,9 +26,21 @@ namespace LibGenerateInfo.Controllers
         }
         public async Task<ActionResult> Book(string id)
         {
-            var s = Request.QueryString;
-            id += s;
-            var book = LibTimeInfo.FromString(id);
+            //TODO: replace with database
+            string base64 = "";
+            var lines = await System.IO.File.ReadAllLinesAsync("a.txt");
+            foreach(var line in lines)
+            {
+                if (line.StartsWith(id + "-"))
+                {
+                    base64 = line.Replace(id + "-", "");
+                    break;
+                }
+            }
+            if (base64 == "")
+                return Content("not found");
+
+            var book = LibTimeInfo.FromString(base64);
             var b = new InfoBook(book.Info);
             await b.GetInfoFromId();
             return Content(" aici apare cartea " + b.Title);
@@ -37,7 +49,10 @@ namespace LibGenerateInfo.Controllers
         {
             var li = new LibTimeInfo(minutes);
             li.Info = id.ToString();
-            
+            //TODO: replace with database
+            var t = Guid.NewGuid().ToString("N");
+            await System.IO.File.AppendAllTextAsync("a.txt", t + "-" + li.Generate());
+            li.Info = t;
             var b = new InfoBook(id.ToString());
             await b.GetInfoFromId();
             
